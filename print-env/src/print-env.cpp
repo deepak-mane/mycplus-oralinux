793b0b663166749cd2a35b1a89662d075b324f3e
Deepak Mane <deepak.mane123@gmail.com>
2018-09-05 08:34:58 +0000

    updated new programs

---
+++ print-env/src/print-env.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : print-env.cpp
+// Author      : Deepak Mane
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/* The ENVIRON variable contains the environment. */
+extern char** environ;
+
+
+int main() {
+	//system("/opt/oracle/psft/pt/ps_home8.56.08/psconfig_new.sh");
+	char** var;
+	for (var = environ; *var != NULL; ++var)
+	printf ("%s\n", *var);
+	return 0;
+
+}
