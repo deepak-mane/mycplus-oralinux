7973e136bd9ff94f153a6dccf9ba182e93049b5e
Deepak Mane <deepak.mane123@gmail.com>
2018-09-07 08:21:47 +0000

    Uploading more programs

--- GetInputFromUser_STRING/src/GetInputFromUser_String.cpp
+++ GetInputFromUser_STRING/src/GetInputFromUser_String.cpp
@@ -6,62 +6,35 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include<iostream>
 #include<sstream>
-
 using namespace std;
 
-//bool isNumber(string s) {
-//    char* p;
-//    strtol(s.c_str(), &p, 10);
-//    return *p == 0;
-//}
-//
-//int getNumber(string s) {
-//    char* p;
-//    return strtol(s.c_str(), &p, 10);
-//}
+int main(int argc, char* argv[])
+{
+  int i, sum = 0;
+  printf("argc = %d\n", argc);
+  printf("Let's see what is in argv[]\n");
 
-int main() {
-	 string strInput;
-	 int str[3];
-	 int cnt;
-	 string arr[] = {"APPDOM", "sstatus", "-c"};
-
-	 do
-	 {
-	  cnt = 0;
-	  cout << "Input 3 values: ";
-	  getline(cin, strInput, '\n');
-
-	  stringstream ss(strInput);
-	  string s;
-
-		 for (int i = 0; i < strInput.length(); i++)
-		 {
-		     if (strInput == arr[i])
-		     {
-		         cout << strInput << " is in the array at position " << i << endl;
-		     }
-		     else
-		     {
-		         cout << strInput << " is not in the array at position " << i << endl;
-		     }
-		 }
-
-		  if(cnt!=3) {
-			  cout<<"Invalid number of inputs!\n";
-		  }
-	 }	while(cnt!=3);
-
-	 int i;
-	 cout<<"The 3 numbers input by the user are: ";
-	 for(i = 0; i < 2; i++)  {
-		 cout<<str[i]<<" ";
-	 }
-	 cout<<endl;
+  if(argc > 1 && argc > 3)
+  {
+    for(i = 1; i < argc; i++)
+    {
+      printf("argv[%d] = %s\n", i, argv[i]);
+      // sum = sum + attoi(argv[i]);
+      sum += atoi(argv[i]);
+    }
+    printf("Total = %d\n", sum);
+  }
+  else {
+    printf("Number Arguments Entered is incorrect\n");
+    printf("Usage: ./add arg1 arg2 arg3\n");
+  }
+  return 0;
 }
+
